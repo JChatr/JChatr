232e2fe73c9f86b0c615dbe398d3957120d2e1ed
Matthias Koch <mk285@hdm-stuttgart.de>
2017-03-24 23:40:02 +0100

    Added method to get plain and hashed ID

--- src/main/java/Chatr/Helper/HashGen.java
+++ src/main/java/Chatr/Helper/HashGen.java
@@ -1,30 +1,48 @@
 package Chatr.Helper;
 
-import java.io.UnsupportedEncodingException;
+
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+
 
 public class HashGen {
-	public static void main(String[] args) {
-		System.out.println(hash("test"));
+
+	/**
+	 * This method gives either an ID in plaintext, or the ID as an MD5 hash.
+	 * @param clear True if you want the ID in plaintext, false when you want it as hash.
+	 * @return The ID as plaintext or as hash.
+	 */
+	public static String getID(boolean clear){
+		String nano = String.valueOf(System.nanoTime());
+		if(clear){
+			return nano;
+		}else{
+			return hash(nano);
+		}
+		
 	}
 	
+	
+	/**
+	 * This methot converts any String into an MD5 hash.
+	 * @param toHash String to be hashed.
+	 * @return The MD5 hash as String.
+	 */
 	public static String hash(String toHash) {
-		try{
-		byte[] msgToByt = toHash.getBytes("UTF-8");
-		MessageDigest md = MessageDigest.getInstance("MD5");
-		byte[] hashByte = md.digest(msgToByt);
-		StringBuffer sb = new StringBuffer();
-		for(int i = 0; i < hashByte.length; i++){
-			sb.append(Integer.toHexString((hashByte[i] & 0xFF) | 0x100).substring(1,3));
-		}
-		return sb.toString();
+		try {
+			byte[] msgToByt = toHash.getBytes("UTF-8");
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			byte[] hashByte = md.digest(msgToByt);
+			StringBuffer sb = new StringBuffer();
+			for (int i = 0; i < hashByte.length; i++) {
+				sb.append(Integer.toHexString((hashByte[i] & 0xFF) | 0x100).substring(1, 3));
+			}
+			return sb.toString();
 		} catch (Exception e) {
-			// TODO: handle exception
+			System.err.println("Error! Couldn't hash the String!");
 		}
-		
-		return "Error!";
-		
+
+		return "";
+
 	}
-	
+
 }
---
+++ src/test/java/Chatr/Helper/HashGenTest.java
@@ -0,0 +1,5 @@
+package Chatr.Helper;
+
+public class HashGenTest {
+
+}
