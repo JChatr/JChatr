895bf203ca36e932ace5ac94ff51eb661be86dc3
Koch Matthias <mk285@hdm-stuttgart.de>
2017-03-27 13:33:21 +0200

    Added java doc to user

--- src/main/java/Chatr/Converstation/User.java
+++ src/main/java/Chatr/Converstation/User.java
@@ -13,22 +13,41 @@ public class User {
 	private String userID = HashGen.getID(false);
 	public String userPicture = "";
 
+	/**
+	 * This method is used to get the user name.
+	 * @return Returns the user name.
+	 */
 	public String getUserName(){
 		return userName;
 	}
 	
+	/**
+	 * This method is used to set the path of the profile path.
+	 */
 	public void setProfileImg(String imgPath){
 		userPicture = imgPath;
 	}
 	
+	/**
+	 * This method is used to get the path of the profile picture.
+	 * @return Path of profile picture.
+	 */
 	public String getProfileImg(){
 		return userPicture;
 	}
 	
+	/**
+	 * This method is used to set the user name.
+	 * @param newName The new user name.
+	 */
 	public void setUserName(String newName){
 		userName = newName;
 	}
 	
+	/**
+	 * This method is used to get the user ID.
+	 * @return Returns the user ID.
+	 */
 	public String getUserID(){
 		return userID;
 	}
