81e2c4b15472d211a790acd355ad67059a4a98dc
Maximilian Merz <maximilianmerz@Maximilians-MacBook-Pro.local>
2017-03-31 08:13:46 +0200

    Java FX eingefügt

--- src/main/java/Chatr/GUI/JavaFx.java
+++ src/main/java/Chatr/GUI/JavaFx.java
@@ -1,4 +1,28 @@
 package Chatr.GUI;
 
-public class JavaFx {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+
+
+public class JavaFx extends Application{
+	
+	@Override
+	public void start(Stage stage) {
+	Label label = new Label("Welcome");
+	StackPane root = new StackPane();
+	root.getChildren().add(label);
+	Scene scene = new Scene(root, 400, 100);
+	scene.setFill(Color.BEIGE);
+	stage.setTitle("First FX");
+	stage.setScene(scene);
+	stage.show();
+	}
+	public static void main(String[] args) {
+	launch(args);
+	}
 }
