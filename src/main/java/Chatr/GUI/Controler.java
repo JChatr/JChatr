5424226af7bea3d82da69dd4b234dff5f3f75006
Maximilian Merz <maximilianmerz@Maximilians-MacBook-Pro.local>
2017-04-11 08:24:40 +0200

    Controllerklasse hinzugefügt

---
+++ src/main/java/Chatr/GUI/Controler.java
@@ -0,0 +1,28 @@
+package Chatr.GUI;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+
+public class Controler {
+	
+//	@FXML
+//	TextArea textArea;
+	
+	@FXML
+	TextField textField;
+	
+	@FXML
+	Label label;
+	
+	@FXML
+	protected void buttonPressed(){
+		String text = textField.getText();
+		label.setText(text);
+		textField.clear();
+	}
+
+}
