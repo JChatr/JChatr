c3f0ce6551d701050d7db4a3bb2527ffb8d7d0b8
Koch Matthias Robert <mk285@hdm-stuttgart.de>
2017-07-03 12:27:16 +0200

    Fixed gif git merge bug

--- src/main/java/Chatr/Helper/GIFLoader.java
+++ src/main/java/Chatr/Helper/GIFLoader.java
@@ -15,6 +15,13 @@ import java.util.concurrent.Executors;
 public class GIFLoader {
 	private static Logger log = LogManager.getLogger(GIFLoader.class);
 
+	/**
+	 * Loads the gif feed from giphy
+	 * @param searchString The search query. If empty get trending gifs
+	 * @param limit The limit of how many gifs you want to load (giphy max is 100)
+	 * @param offset The offset in the searchfeed
+	 * @return Returns the searchfeed
+	 */
 	public static SearchFeed getGIFUrl(String searchString, int limit, int offset) {
 		Giphy giphy = new Giphy("dc6zaTOxFJmzC");
 		SearchFeed feed = null;
@@ -30,6 +37,11 @@ public class GIFLoader {
 		return feed;
 	}
 
+	/**
+	 * Loads an gif as an Objectproperty<Image>
+	 * @param gifImage The Gifimage that should be loaded
+	 * @return Returns gifimage as objectporperty
+	 */
 	public static ObjectProperty<Image> loadGIF(GiphyImage gifImage) {
 		final ObjectProperty<Image> gifObj = new SimpleObjectProperty<>();
 		String urlStr = gifImage.getUrl();
@@ -40,5 +52,55 @@ public class GIFLoader {
 			gifObj.set(gifImg);
 		});
 		return gifObj;
+	}
+
+	/**
+	 * Calculates the width of the gifs in the Searchfeed, so that they align in the flowpane
+	 * @param gifSize The sizes of the gifs in an array
+	 * @param gifFeed The Searchfeed
+	 * @param widthSizeScrollPane The size of the flowpane
+	 * @param feedSize The size of the SearchFeed
+	 * @param gifPaneHGap The HGap of the flowpane
+	 * @return
+	 */
+	public static int[] calcWidth(int[] gifSize, SearchFeed gifFeed, int widthSizeScrollPane, int feedSize, int gifPaneHGap){
+		for (int i = 0; i < feedSize; i++) {
+			gifSize[i] = Integer.valueOf(gifFeed.getDataList().get(i).getImages().getFixedHeightSmall().getWidth());
+		}
+		int maxSize = widthSizeScrollPane;
+		int sizeSum = 0;
+		int indexSum = 0;
+		for (int i = 0; i < feedSize; i++) {
+			maxSize -= (2 * gifPaneHGap);
+			indexSum++;
+			sizeSum += gifSize[i];
+			if (sizeSum > maxSize) {
+				int runter = sizeSum - maxSize;
+				int hoch = maxSize - (sizeSum - gifSize[i]);
+				if (hoch < runter) {
+					sizeSum -= gifSize[i];
+					double scale = (double) sizeSum / maxSize;
+					i--;
+					indexSum--;
+					for (int u = 0; u < indexSum; u++) {
+						gifSize[i - u] = (int) (gifSize[i - u] / scale);
+					}
+					int gifSizeToBig = Integer.valueOf(gifFeed.getDataList().get((i+1)).getImages().getFixedHeight().getWidth());
+					if(gifSizeToBig > widthSizeScrollPane){
+						i++;
+						gifSize[i] = (int) widthSizeScrollPane;
+					}
+				} else {
+					double scale = (double) sizeSum / maxSize;
+					for (int u = 0; u < indexSum; u++) {
+						gifSize[i - u] = (int) (gifSize[i - u] / scale);
+					}
+				}
+				indexSum = 0;
+				sizeSum = 0;
+				maxSize = widthSizeScrollPane;
+			}
+		}
+		return gifSize;
 	}
 }
